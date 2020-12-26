b50d76d554229fd60746585ab6034af72890d412
James Lufz <shot1stop@gmail.com>
2020-12-26 09:53:23 +0700

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const   jwt     =   require('jsonwebtoken')
 const   Port    =   process.env.Port || 3000
 const   app     =   express()
 
-app.get('/api/test',(req,res)=>{
+app.get('/',(req,res)=>{
     res.json({
         message: 'hello'
     })
