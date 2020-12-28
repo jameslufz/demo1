a4b7bdf608d5be6b0ea363c75ed5ee742381e4f5
James Lufz <shot1stop@gmail.com>
2020-12-28 15:03:35 +0700

    new update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const   express =   require('express')
 const   jwt     =   require('jsonwebtoken')
 const   PORT    =   process.env.PORT || 5000
-// const   cors    =   require('cors')
+const   cors    =   require('cors')
 
 const   app     =   express()
 
-// app.use(cors())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.json({
